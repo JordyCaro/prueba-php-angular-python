d3a1c86300e63244d203de6fc9c96da84c0546e0
Jhordan Caro <jhordan_caro@hotmail.com.com>
2024-11-11 10:37:28 -0500

    add script in app

--- backend/api/script-python.php
+++ backend/api/script-python.php
@@ -1,31 +1,29 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 header("Access-Control-Allow-Origin: http://localhost:4200");
 header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Methods: GET");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
+$pythonScript = 'python/python-script.py'; 
 
-$pythonScript = '../../python/script-python.py'; 
+$output = [];
+$return_var = null;
 
 exec("python3 " . escapeshellcmd($pythonScript), $output, $return_var);
 
-if ($return_var === 0) {
-    $jsonFiles = glob("../../python/crypto_prices_*.json"); 
-    usort($jsonFiles, function($a, $b) {
-        return filemtime($b) - filemtime($a); 
+    $prices = array_filter($output, function($line) {
+        return preg_match('/^(Bitcoin|Ethereum):/', $line);
     });
-
-    $latestFile = !empty($jsonFiles) ? $jsonFiles[0] : null;
-
-    if ($latestFile && file_exists($latestFile)) {
-        $jsonData = file_get_contents($latestFile);
-        echo $jsonData;
-    } else {
-        http_response_code(500);
-        echo json_encode(array("mensaje" => "No se pudo encontrar el archivo JSON generado."));
+    
+    $pricesData = [];
+    foreach ($prices as $line) {
+        list($name, $price) = explode(":", $line);
+        $pricesData[trim($name)] = trim($price);
     }
-} else {
-    http_response_code(500);
-    echo json_encode(array("mensaje" => "Error al ejecutar el script de Python."));
-}
+
+    echo json_encode(array("precios" => $pricesData));
 ?>
